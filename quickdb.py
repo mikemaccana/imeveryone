1614e80ed448b304408b0ed5de3d690d3d04f448
Mike MacCana <mike.maccana@gmail.com>
2010-11-25 14:26:18 +0000

    - Disable server starting from quickdb - Disable auto dropping DB from quickdb
    Signed-off-by: Mike MacCana <mike.maccana@gmail.com>

--- quickdb.py
+++ quickdb.py
@@ -8,7 +8,7 @@ config = ConfigObj('imeveryone.conf')
 stage = sys.argv[1]
 db = Database(config,stage=stage)
 
-db.start()
+#db.start()
 db.dbclient()
 db.connection.messages.find_one()
 db.connection.messages.find_one({'_id':1})
@@ -17,7 +17,5 @@ print('_')*80
 print('_')*80
 print('_')*80
 print('_')*80
-#raw_input('DROPPING DATABASE IN 3 SECONDS. Press enter to destroy data, or Ctrl C to cancel')
-time.sleep(3)
-
-db.connection.messages.drop()
+print('Ready')
+#print('''db.connection.messages.drop()''')
