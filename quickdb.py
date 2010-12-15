8d561513e75efc939648a01f438c59c3f1aa2b05
Mike MacCana <mike.maccana@gmail.com>
2010-12-15 10:22:29 +0000

    - Add helpers to get and save messages from DB
    Signed-off-by: Mike MacCana <mike.maccana@gmail.com>

--- quickdb.py
+++ quickdb.py
@@ -22,17 +22,35 @@ print('_')*80
 print('Ready')
 #print('''db.connection.messages.drop()''')
 
+def getmessage(id):
+    '''Get a dict from DB and return a message object'''
+    dbdict = db.connection.messages.find_one({'_id':id})
+    return usermessages.Message(dehydrated=dbdict)
+
+def savemessage(mymessage):
+    '''Save a message object to DB'''
+    quickdb.db.connection.messages.save(mymessage.__dict__)
+
 def getscore(id):
     item = db.connection.messages.find_one({'_id':id})
     print item['_id']+':'+item['posttext']
     print item['score']
     return
 
-def setscore(id):
+def setscore(id,score):
     item = db.connection.messages.find_one({'_id':id})
     print item['_id']+':'+item['posttext']
     print item['score']
+    item['score'] = score
     db.connection.messages.save(item)
+    return
+    
+def updatetreecount(id):
+    '''Update child count on messages'''
+    mymessage = usermessages.Message(dehydrated=db.connection.messages.find_one(id))
+    mymessage.updatetreecount(db.connection)
+    quickdb.db.connection.messages.save(mymessage.__dict__)
+    db.connection.messages.find_one(id).treecount
     return
 
 def updateallitems():
